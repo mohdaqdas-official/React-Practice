6c7bc5bf0cf5038c9aab7ce3910fd0e59aff17d8
mohdaqdas-official <227436609+mohdaqdas-official@users.noreply.github.com>
2026-03-27 22:03:17 +0530

    React-Practice Done

---
+++ React-Practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent'>
+      <h1 id='child1'>Hello</h1>
+      <h2 id='child2'>Yellow</h2>
+    </div>
+  )
+}
+
+export default App
