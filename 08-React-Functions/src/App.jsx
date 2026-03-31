c9e35bbda31bd655570c72b6a262ae2de6f42b76
mohdaqdas-official <227436609+mohdaqdas-official@users.noreply.github.com>
2026-03-31 20:26:12 +0530

    8th Folder Update

--- 08-React-Functions/src/App.jsx
+++ 08-React-Functions/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const App = () => {
+
+  function inputFocus(){
+    console.log("Input Focused");
+  }
+
+  function inputChanging(){
+    console.log("Input value Changed");
+  }
   return (
-    <div>App</div>
+    <div>
+      <input onChange={function(elem){
+        console.log('value: ',elem.target.value, 'Length: ', elem.target.value.length )
+      }}
+      type="text"
+      placeholder='Enter Something' 
+      />
+    </div>
   )
 }
 
