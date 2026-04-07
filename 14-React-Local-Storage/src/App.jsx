20e7961afcd4675167bb0b9b6ff998ed6b87f9e4
mohdaqdas-official <227436609+mohdaqdas-official@users.noreply.github.com>
2026-04-07 18:23:50 +0530

    Local Storage use

---
+++ 14-React-Local-Storage/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  const user = {
+    username: 'Mohd Akdas Ansari',
+    age:19,
+    city: 'Chandigarh',
+  }
+
+  const users = JSON.stringify(localStorage.getItem('user'))
+  const usera = JSON.parse(localStorage.getItem('user'))
+  console.log(usera)
+  
+
+  localStorage.setItem('user', 'Mohd')
+  localStorage.getItem('user')
+  localStorage.removeItem('user')
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
