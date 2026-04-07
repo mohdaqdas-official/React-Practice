7e6c895edb143913cab4206a0e83fcd699cb69c2
mohdaqdas-official <227436609+mohdaqdas-official@users.noreply.github.com>
2026-04-07 17:37:02 +0530

    13-React-Notes-Application-Project Completed

--- 13-React-Notes-Application-Project/src/App.jsx
+++ 13-React-Notes-Application-Project/src/App.jsx
@@ -4,14 +4,13 @@ import NotesCard from './components/NotesCard'
 const App = () => {
 
   const [note, setNote] = useState({title: '', Description: ''})
-  const [error, setError] = useState('')
+  const [error, setError] = useState({Error:'', Style: ''})
   const [task, setTask] = useState([])
 
   const submitHandler = (e) => {
     e.preventDefault()
     if (note.title === '' && note.Description === '') {
-      setError('! Note Cannot be Empty. Please Type Something')
-      console.log('! Note Cannot be Empty. Please Type Something');
+      setError({Error: '! Note Cannot be Empty. Please Type Something', Style: 'bg-red-800 w-full px-2 py-1'})
       
       return
     }
@@ -20,14 +19,20 @@ const App = () => {
     copyTask.push({title: note.title, Description: note.Description})
 
     setTask(copyTask)
-
-    console.log();
-    
+        
     setError('')
     setNote({...note, title: '', Description: ''})
   }
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task]
+    copyTask.splice(idx,1)
+    setTask(copyTask)
+    
+  }
+
   return (
-    <div className='h-screen lg:flex bg-black text-white'>
+    <div className='h-screen lg:flex bg-black text-white overflow-hidden'>
       <form className='flex font-medium lg:w-1/4 w-full flex-col gap-5 p-10 items-start' onSubmit={(e) => {
         submitHandler(e)
       }}>
@@ -42,22 +47,26 @@ const App = () => {
           }}
           />
           <textarea 
-          className='px-5 h-32 py-2 border-2 w-full outline-none rounded'
+          className='px-5 h-32 py-2 border-2 w-full outline-none rounded max-w-md
+          min-h-[40px]
+          max-h-[160px]
+          overflow-y-auto'
           placeholder='Enter Details'  
           value={note.Description}
+          
           onChange={(e) => {
             setNote({ ...note, Description: e.target.value });
           }}
           ></textarea>
           <button className='bg-white active:bg-gray-300 active:scale-95 text-black px-5 py-2 rounded cursor-pointer w-full outline-none'>Add Notes</button>
-          <p className='bg-red-800 w-full px-2 py-1'>{error}</p>
+          <p className={error.Style}>{error.Error}</p>
       </form>
-      <div className='flex flex-col gap-6 p-10 lg:w-3/4 lg: w-full lg:border-l-2 border-t-2'>
-        <h1 className='font-bold text-2xl'>Your Notes</h1>
-        <div className='flex flex-wrap gap-5 h-full overflow-auto'>
-          {/* {task.map(function(){
-            return <div className='h-42 w-42 p-1 rounded-2xl bg-white'></div>
-          })} */}
+      <div className='flex flex-col gap-6 lg:w-3/4 lg: w-full lg:border-l-2 border-t-2 h-full'>
+        <h1 className='font-bold text-2xl px-10 pt-10'>Your Notes</h1>
+        <div className='flex flex-wrap gap-x-5 gap-y-2 px-10 overflow-y-scroll items-start justify-start overflow-x-hidden h-[45%]'>
+          {task.map(function(elem, idx){
+            return <NotesCard key={idx} idx={idx} title={elem.title} Description={elem.Description} onDelete={deleteNote} />
+          })}
         </div>
       </div>
     </div>
--- 13-React-Notes-Application-Project/src/components/NotesCard.jsx
+++ 13-React-Notes-Application-Project/src/components/NotesCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const NotesCard = () => {
+const NotesCard = (props) => {
   return (
-    <div className='h-42 w-42 p-1 rounded-2xl bg-white'>
-        <h1></h1>
-        <p></p>
+    <div idx={props.id} className='flex flex-col justify-between h-52 w-40 bg-cover object-fill p-5 rounded-xl text-black bg-[#ffffff0e] shadow-inner shadow-gray-200'>
+      <div>
+        <h3 className='leading-tight mb-3 font-bold break-all text-lg text-white'>{props.title}</h3>
+        <p className='leading-tight items-start justify-start break-all text-sm font-semibold text-gray-500'>{props.Description}</p>
+      </div>
+      <button onClick={function(){
+        props.onDelete(props.idx)
+      }} className='bg-red-500 text-xl py-1 font-medium text-white rounded-xl outline-none border-none cursor-pointer active:scale-95 hover:shadow-white shadow'>Delete</button>
     </div>
   )
 }
